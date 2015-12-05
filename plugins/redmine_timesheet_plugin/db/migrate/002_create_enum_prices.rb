b83154ff3e7e529033fe5b10eaf50d0d82d5c068
Mario Andrés Correa <a@macool.me>
2015-12-05 07:59:29 -0500

    remove timesheets plugin

--- plugins/redmine_timesheet_plugin/db/migrate/002_create_enum_prices.rb
+++
@@ -1,9 +0,0 @@
-class CreateEnumPrices < ActiveRecord::Migration
-  def change
-    create_table :prices do |t|
-      t.references :enumerations
-      t.float :price
-    end
-    add_index :prices, :enumerations_id
-  end
-end
